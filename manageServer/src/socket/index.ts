f937fb94fd409c15fff2d43cd2c09625bb9f11b5
adxiong <260245435@qq.com>
2022-03-23 22:05:29 +0800

    update

--- manageServer/src/socket/index.ts
+++ manageServer/src/socket/index.ts
@@ -4,7 +4,7 @@
  * @Author: Adxiong
  * @Date: 2022-03-21 15:51:27
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-03-21 15:52:30
+ * @LastEditTime: 2022-03-23 21:38:57
  */
 
 import * as io from 'socket.io'
@@ -15,11 +15,14 @@ interface SocketClientType {
     id: string,
     nick: string
   },
-  roomId: string
 }
 
 export default class SocketServer {
   socket: io.Server
+  superAdmin: {
+    id: string,
+    nick: string
+  }
 
   constructor(server){
     this.socket =  new io.Server(server)
@@ -27,82 +30,66 @@ export default class SocketServer {
   }
   
   registerEventListen () {
-    this.socket.on('connect', (socket) => { 
+    this.socket.on('connect', (socket) => {      
+      console.log(socket);
+       
       socket['userInfo'] = {
-        id: socket.id
-      }   
+        id: socket.id,
+        nick: ""
+      }
       if( !this.socket['client']) this.socket['client'] = []       
       this.socket['client'].push(socket)
-      socket.on('message', (data) => {
+      socket.on('message', (data) => {   
         const message = JSON.parse(data)
         this[message.type](socket,message)
       })
 
       socket.on('disconnect', () => {
         console.log("断开链接===》",socket);
+        //监控平台断开链接，通知所有设备。
+        //设备断开连接，通知监控平台
+        if(socket.id === this.superAdmin.id) {
+          this.socket['client'] = this.socket['client'].filter( client => client['userInfo'].id != socket.id)        
+          this.superAdmin = {
+            id: "",
+            nick: ""
+          }
+          this.dispatchAll({
+            type: "level",
+            payload: {
+              msg: "服务端断开连接，请稍后重新连接"
+            }
+          })
+        } else {
+          //从连接池中删除退出的连接，并通知监控平台
+          this.socket['client'] = this.socket['client'].filter( client => client['userInfo'].id != socket.id)        
+          this.sendToUser(this.superAdmin.id, JSON.stringify({
+            type: 'level',
+            payload: {
+              id: socket['userInfo'].id,
+              nick: socket['userInfo'].nick
+            }
+          }))
+        }
         
-        this.socket['client'] = this.socket['client'].filter( client => client['userInfo'].id != socket.id)        
-        this.boradCaseToRoom(socket['userInfo'], JSON.stringify({
-          type: 'level',
-          userInfo: socket['userInfo'],
-        })) 
-
       })
     }) 
 
   }
 
   join(socket: io.Socket, data) {
-    console.log(data.payload);
-    const userInfo = {
-      id: socket['userInfo'].id,
-      nick: data.payload.nick,
-      roomId: data.payload.roomId
+    //管理平台超级用户进入
+    this.superAdmin = {
+      id: socket.id,
+      nick: data.nick
     }
-    logger.info(`id=${userInfo.id} 的 ===》 ${userInfo.nick} 加入房间 ${userInfo.roomId}`)
-
-    socket['userInfo'] = userInfo
-        
-    const roomUserList = this.getRoomUserList(userInfo.roomId, userInfo)
-    
-    socket.send( 
-      JSON.stringify(      {
-        type: "roomInfo",
-        payload: {
-          users: roomUserList,
-          userInfo
-        }
-      } )
-    )
-
-    this.boradCaseToRoom(userInfo,JSON.stringify(      {
-      type: "newUserJoin",
-      payload: {},
-      userInfo
-    } )
-    )
+    socket['userInfo'].nick = data.nick
+    logger.info(`id=${this.superAdmin.id} 的 ===》${this.superAdmin.nick} 加入`)
   }
 
-  getRoomUserList (roomId,userInfo) {    
-    return Array.from(this.socket['client'])
-    .map( client => client['userInfo'])
-    .filter( userInfo => userInfo.roomId == roomId)
-    .filter(user => user.id !== userInfo.id)
-  }
-
-  sendToRoom (userInfo, data) {
-    this.socket['client'].forEach( client => {
-      if (client['userInfo']['roomId'] == userInfo.roomId ) {        
-        client.send(JSON.stringify(data))
-      }
-    });
-  }
-
-  boradCaseToRoom (userInfo, data) {
-    this.socket['client'].forEach( client => {
-      if (client['userInfo']['roomId'] == userInfo.roomId && client['userInfo']['id'] != userInfo.id ) {        
-        client.send(data)
-      }
+  dispatchAll (data) {
+    this.socket['client'].forEach(client => {
+      client.send(JSON.stringify(data))
     });
   }
 
@@ -114,34 +101,25 @@ export default class SocketServer {
     });
   }
 
-  /**
-   * 加入房间，房间已存在用户大于1，新加入的用户发送offer 
-   * 
-   * 发送者 createOffer => setLocalDescription => sendOffer
-   * 
-   * 接受者 setRemoteDescription => createAnswer => setLocalDescription => sendAnswer
-   * 
-   * 收到offer 给该房间内除自己以外的用户转发offer  「 sendId , offer 」
-   * 收到answer 返回给发送offer的人     [ receiveId, answer]
-   */
-
-  offer (socket, message) {
-    
-    this.sendToUser(message.receiveId, {
+  offer (socket, message) {    
+    socket['userInfo'].nick = message.nick
+    this.sendToUser(this.superAdmin.id, {
       ...message,
       userInfo: socket['userInfo']
     })
   }
 
   answer (socket, message) {    
-    this.sendToUser( message.receiveId, {
+    this.sendToUser( message.id, {
       ...message,
       userInfo: socket['userInfo']
     })
   }
 
   icecandidate (socket, message) {    
-    this.sendToUser( message.receiveId, {
+    console.log(message);
+    
+    this.sendToUser( message.id, {
       ...message,
       userInfo: socket['userInfo']
     })
